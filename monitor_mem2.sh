209749e712430667fcf1483b61eb17f6c496217d
james <james@shi.(none)>
2013-01-09 17:12:41 +0800

    monitor_mem2.sh

---
+++ monitor_mem2.sh
@@ -0,0 +1,21 @@
+#watch memery is any process eat mem more than 5M echo it to mem_log.txt
+#it works as cron job
+#!/bin/bash
+function watch_mem()
+{
+while :
+do 
+ps aux | awk '$4>5{"date"|getline date ;print date;printf "the process ID is : %d\n",$2;print $0 }' >> ./mem_log.txt;
+sleep 5;
+
+done;
+}
+count=`pgrep monitor_mem.sh | wc -l`
+#echo $count
+if [ $count -eq 2 ];then
+	count=0
+	watch_mem
+else
+	exit
+fi
+#exit
